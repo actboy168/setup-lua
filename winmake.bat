a715aec570f9b93b95eb0badbb6cd9d32b151ed1
actboy168 <actboy168@gmail.com>
2024-03-27 22:04:25 +0800

    update winmake

--- winmake.bat
+++ winmake.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
    SET InstallDir=%%i
 )
 call "%InstallDir%\Common7\Tools\vsdevcmd.bat" -no_logo -arch=x64 -host_arch=x64
@@ -16,7 +16,7 @@ CD %SOURCE%
 SET OBJS=
 for %%f in (*.c) do ( 
    SET FILENAME=%%~nf
-   if not "!FILENAME!" == "lua" if not "!FILENAME!" == "luac" if not "!FILENAME!" == "print" (
+   if not "!FILENAME!" == "lua" if not "!FILENAME!" == "luac" if not "!FILENAME!" == "print" if not "!FILENAME!" == "onelua" if not "!FILENAME!" == "ltests" (
       SET OBJS=!OBJS! !FILENAME!.obj
       SET COMPCMD=cl /nologo /MD /O2 /W3 /c /D_CRT_SECURE_NO_DEPRECATE /DLUA_BUILD_AS_DLL %CFLAG% !FILENAME!.c
       echo !COMPCMD!
